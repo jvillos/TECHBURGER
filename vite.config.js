237edb1ceb5dcccf2242ab06e648bec5bb65b582
jvillos <jvillosl14@gmail.com>
2024-11-28 14:04:33 +0100

    intento fonts-parrafos

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src", // Cambia la raíz del proyecto a "src"
+  base: '/TECHBURGER/',//esta parte es necesaria para que github pages importe bien
   build: {
     outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
   },
